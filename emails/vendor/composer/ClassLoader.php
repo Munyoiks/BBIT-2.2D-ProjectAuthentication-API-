301fe538f30f917f45371f1e7d7ca7909f648b20
ShethDavis <Davis.Kirathe@strathmore.edu>
2025-09-24 15:57:06 +0300

    Download of dependancies

--- emails/vendor/composer/ClassLoader.php
+++ emails/vendor/composer/ClassLoader.php
@@ -215,6 +215,7 @@ class ClassLoader
         }
     }
 
+    
     /**
      * Registers a set of PSR-4 directories for a given namespace, either
      * appending or prepending to the ones previously set for this namespace.
